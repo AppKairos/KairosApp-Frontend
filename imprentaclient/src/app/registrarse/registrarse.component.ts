06c05dbeedde87a7204d73bfd44b3cc16cc3a22c
unknown <gustavogud77@gmail.com>
2021-10-14 08:17:02 -0400

    validacion de formularios login y registrarse, borde agregado a imagen empastado, mes input reemplazado por select en empastado

--- imprentaclient/src/app/registrarse/registrarse.component.ts
+++ imprentaclient/src/app/registrarse/registrarse.component.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 import { RegistrarseService } from '../services/registrarse.service';
 import { Usuario } from '../models/Usuario';
 import { Response } from '../models/response';
-import { FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
@@ -16,11 +16,26 @@ import { Router, RouterModule, Routes } from '@angular/router';
 export class RegistrarseComponent implements OnInit {
 
   profileForm = new FormGroup({
-    ci: new FormControl(),
-    nombre: new FormControl(''),
-    correo: new FormControl(''),
-    telefono: new FormControl(),
-    password: new FormControl('')
+    ci: new FormControl(0,[
+      Validators.required,
+      Validators.pattern(new RegExp(/^[\d]{7,8}$/))
+    ]),
+    nombre: new FormControl('',[
+      Validators.required,
+      Validators.pattern(new RegExp( /^([A-Za-z,.'-]{2,12}\s?){1,}/))
+    ]),
+    correo: new FormControl('',[
+      Validators.required,
+      Validators.pattern(new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/))
+    ]),
+    telefono: new FormControl(0,[
+      Validators.required,
+      Validators.pattern(new RegExp(/^([0-9]{8,8})$|^([0-9]{5,5})$/))
+    ]),
+    password: new FormControl('',[
+      Validators.required,
+      Validators.pattern(new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/))
+    ])
   });
 
   constructor(private registrarseService: RegistrarseService, private modalservice: NgbModal, private router: Router) { }
@@ -35,5 +50,10 @@ export class RegistrarseComponent implements OnInit {
       alert('Registrado correctamente');
       this.router.navigate(['/login']);
     });
+  }
+
+  validarCampo(campo:string):string {
+    const campoValidado = this.profileForm.get(campo);
+    return (!campoValidado.valid && campoValidado.touched) ? 'is-invalid' : campoValidado.touched ? 'is-valid' : '';
   }
 }
